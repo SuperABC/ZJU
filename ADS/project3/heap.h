bc9374178e4a825a461052a833055e4c99d4ed62
SG master <1363028533@qq.com>
2017-04-03 14:18:39 +0800

    Add files via upload

--- ADS/project3/heap.h
+++ ADS/project3/heap.h
@@ -1,23 +1,25 @@
-#pragma once
 #ifndef HEAP_H
 #define HEAP_H
 
 #define MAX_SIZE 300000
+#define INFINITY 1000000000
 
 struct HeapNode {
 	int index;
 	int distance;
+	HeapNode(int i) : index(i), distance(INFINITY) {};
 };
 
 class Heap {
 public:
 	Heap();
 	~Heap();
-	virtual void insert(int index, int distance) = 0;
 	virtual int get() = 0;
 	virtual void decrease(int index, int distance) = 0;
-private:
-	HeapNode queue[MAX_SIZE];
+protected:
+	HeapNode *queue[MAX_SIZE];
+	int identifier[MAX_SIZE];
+	int nodeNum, tmpNum;
 };
 
 #endif
