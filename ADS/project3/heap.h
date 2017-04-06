040b52425789478438acce966bd787f128567163
celestialphineas <hanghang0713@hotmail.com>
2017-04-06 21:14:51 +0800

    Change private to protected. Remove the heapify method (better to implement as a static member of a derived class)

--- ADS/project3/heap.h
+++ ADS/project3/heap.h
@@ -8,10 +8,10 @@
 
 template<class T>
 void swap(T *a, T *b) {
-	T tmp;
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+    T tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
 // Since the heap is essentially priority queue, it shound not allow random
@@ -39,81 +39,78 @@ void swap(T *a, T *b) {
 //		- percolate-down
 template<class Type>
 class MinHeap {
-	public:
-		// Rule of three
-		MinHeap() {n_element = 0;}
-		MinHeap(MinHeap<Type> &src) {n_element = src.n_element;}
-		virtual ~MinHeap() = 0;
-		virtual MinHeap<Type>& operator=(MinHeap<Type> &src)
-			{n_element = src.n_element;}
-		// find-min/get-top
-		virtual Type get_top() const = 0;
-		Type find_min() const {return get_top();}
-		// merge/operator+
-		// The merge method MUST return A NEW INSTANCE
-		// ** Don't forget to evaluate new SIZE **
-		virtual MinHeap<Type> merge(const MinHeap<Type>&) const = 0;
-		MinHeap<Type> operator+(const MinHeap<Type> &another_heap) const
-			{return merge(another_heap);}
-		// meld/operator+=, return *this
-		// The meld method MUST NOT return a new instance, its behavior should
-		// be to merge the argument into this heap
-		// ** Don't forget to evaluate new SIZE **
-		virtual MinHeap<Type> meld(const MinHeap<Type> &src) {return *this;}
-		MinHeap<Type> operator+=(const MinHeap<Type> &another_heap)
-			{return meld(another_heap);}
-		// push
-		// Return value is the new top.
-		// ** Don't forget to evaluate new SIZE **
-		virtual Type push(const Type&);
-		// pop
-		// Return value is the new top.
-		// ** Don't forget to evaluate new SIZE **
-		virtual Type pop() = 0;
-		// replace_top
-		// Return value is the new top.
-		// Note that the implementation of the replace method should not be a
-		// simple combination of push and pop. There are usually more efficiency
-		// algorithms (only one percolation is required).
-		virtual Type replace_top(Type&) = 0;
-		Type replace(Type& replacing) {return replace_top(replacing);}
-		Type replace_min(Type& replacing) {return replace_top(replacing);}
-		// size
-		unsigned size() const {return n_element;}
-		unsigned length() const {return n_element;}
-		// is-empty
-		bool is_empty() const {return !!n_element;}
-		// heapify
-		virtual MinHeap<Type>& heapify(const Type* src, int length) = 0;
-		virtual MinHeap<Type>& heapify(const std::vector<Type>& src) = 0;
-	private:
-		// Number of the elements
-		unsigned n_element;
+    public:
+        // Rule of three
+        MinHeap() {n_element = 0;}
+        MinHeap(MinHeap<Type> &src) {n_element = src.n_element;}
+        virtual ~MinHeap() = 0;
+        virtual MinHeap<Type>& operator=(MinHeap<Type> &src)
+            {n_element = src.n_element;}
+        // find-min/get-top
+        virtual Type get_top() const = 0;
+        Type find_min() const {return get_top();}
+        // merge/operator+
+        // The merge method MUST return A NEW INSTANCE
+        // ** Don't forget to evaluate new SIZE **
+        virtual MinHeap<Type> merge(const MinHeap<Type>&) const = 0;
+        MinHeap<Type> operator+(const MinHeap<Type> &another_heap) const
+            {return merge(another_heap);}
+        // meld/operator+=, return *this
+        // The meld method MUST NOT return a new instance, its behavior should
+        // be to merge the argument into this heap
+        // ** Don't forget to evaluate new SIZE **
+        virtual MinHeap<Type> meld(const MinHeap<Type> &src) {return *this;}
+        MinHeap<Type> operator+=(const MinHeap<Type> &another_heap)
+            {return meld(another_heap);}
+        // push
+        // Return value is the new top.
+        // ** Don't forget to evaluate new SIZE **
+        virtual Type push(const Type&);
+        // pop
+        // Return value is the new top.
+        // ** Don't forget to evaluate new SIZE **
+        virtual Type pop() = 0;
+        // replace_top
+        // Return value is the new top.
+        // Note that the implementation of the replace method should not be a
+        // simple combination of push and pop. There are usually more efficiency
+        // algorithms (only one percolation is required).
+        virtual Type replace_top(Type&) = 0;
+        Type replace(Type& replacing) {return replace_top(replacing);}
+        Type replace_min(Type& replacing) {return replace_top(replacing);}
+        // size
+        unsigned size() const {return n_element;}
+        unsigned length() const {return n_element;}
+        // is-empty
+        bool is_empty() const {return !!n_element;}
+    protected:
+        // Number of the elements
+        unsigned n_element;
 };
 
 struct HeapNode {
-	int index;
-	int distance;
-	HeapNode(int i = 0) : index(i), distance(INFINITY) {};
-	~HeapNode() {};
-	HeapNode operator=(HeapNode h) {
-		index = h.index;
-		distance = h.distance;
-		return *this;
-	}
+    int index;
+    int distance;
+    HeapNode(int i = 0) : index(i), distance(INFINITY) {};
+    ~HeapNode() {};
+    HeapNode operator=(HeapNode h) {
+        index = h.index;
+        distance = h.distance;
+        return *this;
+    }
 };
 
 class Heap {
 public:
-	Heap();
-	virtual ~Heap();
-	virtual int get() = 0;
-	virtual void decrease(int index, int distance) = 0;
+    Heap();
+    virtual ~Heap();
+    virtual int get() = 0;
+    virtual void decrease(int index, int distance) = 0;
 protected:
-	HeapNode *queue[MAX_SIZE];
-	HeapNode *dynQueue[MAX_SIZE];
-	int identifier[MAX_SIZE];
-	int nodeNum, tmpNum;
+    HeapNode *queue[MAX_SIZE];
+    HeapNode *dynQueue[MAX_SIZE];
+    int identifier[MAX_SIZE];
+    int nodeNum, tmpNum;
 };
 
 #endif
