2458d33a464dff0fa7dffc2ccff9a752b4700714
SG master <1363028533@qq.com>
2017-12-07 15:23:12 +0800

    Update.

--- NN/RBRDF/gradient.h
+++ NN/RBRDF/gradient.h
@@ -5,13 +5,13 @@
 #include "glm/vec2.hpp"
 #include "glm/matrix.hpp"
 
-#define DELTA 0.0001f
+#define DELTA 0.00001f
 #define RATE .8f
 
 extern cam_coord_system Cam;
 extern light_system Light;
 
-float para[7] = { .5f, .5f, .5f, .5f, .5f, .5f, .5f };
+float para[7] = { .4f, .4f, .1f, .1f, .1f, .1f, .1f };
 
 float clamp(float floor, float ceil, float d) {
 	if (d < floor)return floor;
@@ -24,6 +24,7 @@ float variant(std::vector<float> &v1, std::vector<float> &v2) {
 #pragma omp parallel for
 	for (int i = 0; i < LIGHT_SIZE; i++) {
 		res += (v1[i] - v2[i])*(v1[i] - v2[i]);
+		//res += fabs(v1[i] - v2[i]);
 	}
 	return res;
 }
@@ -36,7 +37,7 @@ float expand(std::vector<float> &tmp, std::vector<float> &pre, std::vector<float
 	}
 	return res;
 }
-bool gradient(std::vector<glm::vec3> &wi, glm::vec3 &wo, std::vector<float> &brdf, float *res, bool r = false) {
+bool gradient(std::vector<glm::vec3> &wi, glm::vec3 &wo, int x, int y, std::vector<float> &brdf, float *res, bool r = false) {
 	float dn1, dn2, dt, dax, day, dpd, dps;
 	float odn1 = 0, odn2 = 0, odt = 0, odax = 0, oday = 0, odpd = 0, odps = 0;
 	float vn1 = 0, vn2 = 0, vt = 0, vax = 0, vay = 0, vpd = 0, vps = 0;
@@ -44,20 +45,31 @@ bool gradient(std::vector<glm::vec3> &wi, glm::vec3 &wo, std::vector<float> &brd
 	int pn1 = 0, pn2 = 0, pt = 0, pax = 0, pay = 0, ppd = 0, pps = 0;
 
 
-	float step = 0.01f;
+	float step = 0.02f;
 
 	std::vector<float> pre, tmp;
 	float var, min = INFINITE, round = 0, len;
 
 	if (r) {
-		para[0] = para[1] = para[2] = para[3] = para[4] = para[5] = para[6] = (float(rand() % 10)) / 10;
+		para[0] = para[1] = para[2] = para[3] = para[4] = para[5] = para[6] = (float(rand()%10))/10;
 	}
 	else {
-		para[0] = para[1] = para[2] = para[3] = para[4] = para[5] = para[6] = 0.5;
+		//para[0] = para[1] = para[2] = para[3] = para[4] = para[5] = para[6] = 0.5;
 	}
 
+	clock_t t = clock();
 	while (1) {
-		light_brdf(1386, 1096, Cam, Light, para, pre);
+
+#ifndef DEBUG
+		if (clock() - t > 2000) {
+			if(var>5e-13){
+				std::cout << "retry ";
+			}
+			break;
+		}
+#endif
+
+		light_brdf(x, y, Cam, Light, para, pre);
 		var = variant(pre, brdf);
 		if (var < min) {
 			min = var;
@@ -66,41 +78,44 @@ bool gradient(std::vector<glm::vec3> &wi, glm::vec3 &wo, std::vector<float> &brd
 		}
 		else {
 			round++;
-			if (round > 100)break;
+			if (round > 10) {
+				step /= 2;
+				round = 0;
+			}
 		}
 
 		para[0] += DELTA;
-		light_brdf(1386, 1096, Cam, Light, para, tmp);
+		light_brdf(x, y, Cam, Light, para, tmp);
 		dn1 = (variant(tmp, brdf) - var) / DELTA;
 		if (dn1 == 0) dn1 = expand(tmp, pre, brdf) / DELTA;
 		para[0] -= DELTA;
 		para[1] += DELTA;
-		light_brdf(1386, 1096, Cam, Light, para, tmp);
+		light_brdf(x, y, Cam, Light, para, tmp);
 		dn2 = (variant(tmp, brdf) - var) / DELTA;
 		if (dn2 == 0) dn2 = expand(tmp, pre, brdf) / DELTA;
 		para[1] -= DELTA;
 		para[2] += DELTA;
-		light_brdf(1386, 1096, Cam, Light, para, tmp);
+		light_brdf(x, y, Cam, Light, para, tmp);
 		dt = (variant(tmp, brdf) - var) / DELTA;
 		if (dt == 0) dt = expand(tmp, pre, brdf) / DELTA;
 		para[2] -= DELTA;
 		para[3] += DELTA;
-		light_brdf(1386, 1096, Cam, Light, para, tmp);
+		light_brdf(x, y, Cam, Light, para, tmp);
 		dax = (variant(tmp, brdf) - var) / DELTA;
 		if (dax == 0) dax = expand(tmp, pre, brdf) / DELTA;
 		para[3] -= DELTA;
 		para[4] += DELTA;
-		light_brdf(1386, 1096, Cam, Light, para, tmp);
+		light_brdf(x, y, Cam, Light, para, tmp);
 		day = (variant(tmp, brdf) - var) / DELTA;
 		if (day == 0) day = expand(tmp, pre, brdf) / DELTA;
 		para[4] -= DELTA;
 		para[5] += DELTA;
-		light_brdf(1386, 1096, Cam, Light, para, tmp);
+		light_brdf(x, y, Cam, Light, para, tmp);
 		dpd = (variant(tmp, brdf) - var) / (DELTA * 20);
 		if (dpd == 0) dpd = expand(tmp, pre, brdf) / (DELTA * 20);
 		para[5] -= DELTA;
 		para[6] += DELTA;
-		light_brdf(1386, 1096, Cam, Light, para, tmp);
+		light_brdf(x, y, Cam, Light, para, tmp);
 		dps = (variant(tmp, brdf) - var) / DELTA;
 		if (dps == 0) dps = expand(tmp, pre, brdf) / DELTA;
 		para[6] -= DELTA;
@@ -120,7 +135,7 @@ bool gradient(std::vector<glm::vec3> &wi, glm::vec3 &wo, std::vector<float> &brd
 		if (para[6] == 0.f&&dps > 0)dps = 0;
 		if (para[6] == 1.f&&dps < 0)dps = 0;
 
-		if (pn1)dn1 /= 1 << (pn1 * 2);
+		if (pn1)dn1 /= 1<<(pn1 * 2);
 		if (pn2)dn2 /= 1 << (pn2 * 2);
 		if (pt)dt /= 1 << (pt * 2);
 		if (pax)dax /= 1 << (pax * 2);
@@ -138,34 +153,41 @@ bool gradient(std::vector<glm::vec3> &wi, glm::vec3 &wo, std::vector<float> &brd
 		dpd /= len;
 		dps /= len;
 
-		if (odn1 > 0.5&&dn1 < -0.5&&abs(odn1 + dn1)<.01f)
-			if (var<1e-11)pn1 += 3;
+		if (odn1 > 0.5&&dn1 < -0.5&&abs(odn1+dn1)<.01f)
+			if(var<1e-11)pn1+=3;
 			else if (var < 5e-9)pn1 += 2;
 			else pn1++;
+		//else pn1?pn1--:0;
 		if (odn2 > 0.5&&dn2 < -0.5&&abs(odn2 + dn2)<.01f)
 			if (var<1e-11)pn2 += 3;
 			else if (var < 5e-9)pn2 += 2;
 			else pn2++;
+		//else pn2 ? pn2-- : 0;
 		if (odt > 0.5&&dt < -0.5&&abs(odt + dt)<.01f)
 			if (var<1e-11)pt += 3;
 			else if (var < 5e-9)pt += 2;
 			else pt++;
+		//else pt ? pt-- : 0;
 		if (odax > 0.5&&dax < -0.5&&abs(odax + dax)<.01f)
 			if (var<1e-11)pax += 3;
 			else if (var < 5e-9)pax += 2;
 			else pax++;
+		//else pax ? pax-- : 0;
 		if (oday > 0.5&&day < -0.5&&abs(oday + day)<.01f)
 			if (var<1e-11)pay += 3;
 			else if (var < 5e-9)pay += 2;
 			else pay++;
+		//else pay ? pay-- : 0;
 		if (odpd > 0.5&&dpd < -0.5&&abs(odpd + dpd)<.01f)
 			if (var<1e-11)ppd += 3;
 			else if (var < 5e-9)ppd += 2;
 			else ppd++;
+		//else ppd ? ppd-- : 0;
 		if (odps > 0.5&&dps < -0.5&&abs(odps + dps)<.01f)
 			if (var<1e-11)pps += 3;
 			else if (var < 5e-9)pps += 2;
 			else pps++;
+		//else pps ? pps-- : 0;
 
 		odn1 = dn1;
 		odn2 = dn2;
@@ -200,27 +222,37 @@ bool gradient(std::vector<glm::vec3> &wi, glm::vec3 &wo, std::vector<float> &brd
 		para[0] = clamp(0.f, 1.f, para[0]);
 		para[1] = clamp(0.f, 1.f, para[1]);
 		para[2] = clamp(0.f, 1.f, para[2]);
-		para[3] = clamp(0.f, 1.f, para[3]);
-		para[4] = clamp(0.f, 1.f, para[4]);
+		para[3] = clamp(0.0001f, 1.f, para[3]);
+		para[4] = clamp(0.0001f, 1.f, para[4]);
 		para[5] = clamp(0.f, 1.f, para[5]);
 		para[6] = clamp(0.f, 1.f, para[6]);
 
+#ifdef DEBUG
 		std::cout << dn1 << " " << dn2 << " " << dt << " " << dax << " " << day << " " << dpd << " " << dps << std::endl <<
 			para[0] << " " << para[1] << " " << para[2] << " " << para[3] << " " << para[4] << " " << para[5] << " " << para[6] << std::endl <<
 			var << std::endl << std::endl;
+#endif
 
-		if (var < 1e-11) {
-			if (step == 0.01f)
+		if (var < 1e-1) {
+			if (step >= 0.02f)
+				step = 0.01f;
+		}
+		if (var < 1e-2) {
+			if (step >= 0.01f)
 				step = 0.001f;
 		}
-		if (var < 3e-13) {
-			if (step == 0.001f)
+		if (var < 1e-3) {
+			if (step >= 0.001f)
 				step = 0.0001f;
 		}
-		if (var < 5e-14)
+		if (var < 1e-4) {
+			if (step >= 0.0001f)
+				step = 0.00001f;
+		}
+		if (var < 5e-5)
 			break;
 	}
-	if (var > 2e-13)return true;
+	if (var > 5e-4)return true;
 	else {
 		std::ofstream fouttmp, foutstd;
 		fouttmp.open("outtmp.txt", 0);
