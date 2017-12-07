d0feb7291147cb781a2eb0003ab7533300585c88
SG master <1363028533@qq.com>
2017-12-07 15:23:17 +0800

    Update.

--- NN/RBRDF/main.cpp
+++ NN/RBRDF/main.cpp
@@ -10,6 +10,8 @@ para_brdf Data;
 cam_coord_system Cam;
 light_system Light;
 
+int x = 1368, y = 1096;
+
 glm::vec3 normalize(glm::vec3 v) {
 	float len = sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
 	return glm::vec3(v.x / len, v.y / len, v.z / len);
@@ -22,6 +24,7 @@ int main() {
 	read_cam_and_light("cam_and_light.txt", Cam, Light);
 
 	FILE *fp;
+	//fopen_s(&fp, "tmp.txt", "rb+");
 	fopen_s(&fp, "test.txt", "rb+");
 
 	int size;
@@ -35,22 +38,34 @@ int main() {
 	std::vector<glm::vec3>wi;
 	glm::vec3 wo;
 
-	compute_wiwo(1386, 1096, Cam, Light, wi, wo);
+	compute_wiwo(x, y, Cam, Light, wi, wo);
 
+	clock_t g = clock();
 	std::vector<para_brdf> para_res(size);
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < 1000; i++) {
 		clock_t t = clock();
-		if (gradient(wi, wo, para_value[i].get_brdf(), para_res[i].get_para())) {
-			while (gradient(wi, wo, para_value[i].get_brdf(), para_res[i].get_para(), true));
+		if (gradient(wi, wo, x, y, para_value[i].get_brdf(), para_res[i].get_para())) {
+			while (gradient(wi, wo, x, y, para_value[i].get_brdf(), para_res[i].get_para(), true)) {
+
+#ifndef DEBUG
+				if (clock() - t > 4000) {
+					std::cout << "fail ";
+					break;
+				}
+#endif 
+
+			}
 		}
 		std::cout << clock() - t << std::endl;
+
+#ifdef DEBUG
 		std::cout << para_value[i].get_para()[0] << " " << 
-			para_value[i].get_para()[1] << " " <<
-			para_value[i].get_para()[2] << " " <<
-			para_value[i].get_para()[3] << " " <<
-			para_value[i].get_para()[4] << " " <<
-			para_value[i].get_para()[5] << " " <<
-			para_value[i].get_para()[6] << std::endl <<
+			para_res[i].get_para()[1] << " " <<
+			para_res[i].get_para()[2] << " " <<
+			para_res[i].get_para()[3] << " " <<
+			para_res[i].get_para()[4] << " " <<
+			para_res[i].get_para()[5] << " " <<
+			para_res[i].get_para()[6] << std::endl <<
 			para_value[i].get_para()[0] - para_res[i].get_para()[0] << " " <<
 			para_value[i].get_para()[1] - para_res[i].get_para()[1] << " " <<
 			para_value[i].get_para()[2] - para_res[i].get_para()[2] << " " <<
@@ -59,8 +74,12 @@ int main() {
 			para_value[i].get_para()[5] - para_res[i].get_para()[5] << " " <<
 			para_value[i].get_para()[6] - para_res[i].get_para()[6] << std::endl << std::endl;
 
-		//system("pause");
+		system("pause");
+#endif
+
 	}
+	std::cout << clock() - g << std::endl;
+	system("pause");
 
 	return 0;
 }
