9f9926158577e482b9d3650f689ab4cdf7e170a1
SG master <1363028533@qq.com>
2017-12-27 12:04:22 +0800

    Init.

---
+++ JAVA/Frame/Main.java
@@ -0,0 +1,23 @@
+package Frame;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+	public static String srcPath = "C:\\Users\\MLKJ\\eclipse-workspace\\CSK\\src\\Source\\drawable\\";
+	
+	public static Table frame;
+	
+	public static void main(String[] args) {
+	    EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				frame = new Table();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	            frame.setTitle("CS Killer");
+				frame.setVisible(true);
+			}
+	    });
+	}
+}
+
